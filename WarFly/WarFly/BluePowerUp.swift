7976ce59e4a3c874eb6f8bb1b3f1f3431c26a8db
Andriy <andriy@MacBook-Pro-Admin.local>
2022-04-18 01:48:09 +0300

    Commit Atlas

---
+++ WarFly/WarFly/BluePowerUp.swift
@@ -0,0 +1,21 @@
+//
+//  BluePowerUp.swift
+//  WarFly
+//
+//  Created by Ivan Akulov on 13/05/2017.
+//  Copyright © 2017 Ivan Akulov. All rights reserved.
+//
+
+import SpriteKit
+
+class BluePowerUp: PowerUp {
+    
+    init() {
+        let textureAtlas = Assets.shared.bluePowerUpAtlas
+        super.init(textureAtlas: textureAtlas)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
